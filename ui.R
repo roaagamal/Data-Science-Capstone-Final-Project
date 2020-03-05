18bec194ca594dc582da5bdd7ba4ab96b81c7185
Roa'a Gamal <40736139+roaagamal@users.noreply.github.com>
2020-03-05 20:49:10 +0200

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(shinythemes)
+library(markdown)
+library(dplyr)
+library(tm)
+
+shinyUI(
+    navbarPage("Next Word Predict",
+               theme = shinytheme("spacelab"),
+               tabPanel("",
+                        fluidPage(
+                            titlePanel(" "),
+                            sidebarLayout(
+                                sidebarPanel(
+                                    textInput("userInput",
+                                              "Enter a word or phrase:",
+                                              value =  "",
+                                              placeholder = "Enter text here"),
+                                    br(),
+                                    sliderInput("numPredictions", "Number of Predictions:",
+                                                value = 1.0, min = 1.0, max = 3.0, step = 1.0)
+                                ),
+                                mainPanel(
+                                    h4("Input text"),
+                                    verbatimTextOutput("userSentence"),
+                                    br(),
+                                    h4("Predicted words"),
+                                    verbatimTextOutput("prediction1"),
+                                    verbatimTextOutput("prediction2"),
+                                    verbatimTextOutput("prediction3")
+                                )
+                            )
+                        )
+               )
+    )
+)
